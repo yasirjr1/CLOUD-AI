99e2742be3554653ee548f184c316945b25ff334
DEVELOPER-BERA <berabruce9@gmail.com>
2025-03-22 00:33:40 +0300

    Update py.js

--- plugins/py.js
+++ plugins/py.js
@@ -20,17 +20,36 @@ const play = async (_0x1b9510, _0xde7a32) => {
       }
       
       const _0x24d96b = _0x589357.videos[0x0];
-      const _0x384e8c = `\n\n╭━━━〔 *ᴄʟᴏᴜᴅ ᴀɪ ᴅᴏᴡɴʟᴏᴅᴇʀ* 〕━━━\n\n┃▸ *Title:* ${_0x24d96b.title}\n\n┃▸ *Duration:* ${_0x24d96b.timestamp}\n\n┃▸ *Views:* ${_0x24d96b.views}\n\n┃▸ *Channel:* ${_0x24d96b.author.name}\n\n╰━━━━━━━━━━━━━━━━━━\n\n📥 *Downloading automatically...*`;
+      const _0x384e8c = `\n\n╭━━━〔 *ᴄʟᴏᴜᴅ ᴀɪ ᴅᴏᴡɴʟᴏᴅᴇʀ* 〕━━━\n\n┃▸ *Title:* ${_0x24d96b.title}\n\n┃▸ *Duration:* ${_0x24d96b.timestamp}\n\n┃▸ *Views:* ${_0x24d96b.views}\n\n┃▸ *Channel:* ${_0x24d96b.author.name}\n\n╰━━━━━━━━━━━━━━━━━━\n\n📥 
+      *> regards Bruce Bera*
+      *Downloading automatically...*`;
 
       await _0xde7a32.sendMessage(_0x1b9510.from, { 
         image: { url: _0x24d96b.thumbnail }, 
         caption: _0x384e8c 
       }, { quoted: _0x1b9510 });
 
-      const _0xac0071 = _0x24d96b.url;
+      const _0xac0071 = encodeURIComponent(_0x24d96b.url);
+
+      // Define MP4 & MP3 API sources
+      const _0xapiMp4 = [
+        `https://api-rin-tohsaka.vercel.app/download/ytmp4?url=${_0xac0071}`,
+        `https://apis.davidcyriltech.my.id/download/ytmp4?url=${_0xac0071}`,
+        `https://www.dark-yasiya-api.site/download/ytmp4?url=${_0xac0071}`,
+        `https://api.giftedtech.web.id/api/download/dlmp4?url=${_0xac0071}&apikey=gifted-md`
+      ];
+
+      const _0xapiMp3 = [
+        `https://api-rin-tohsaka.vercel.app/download/ytmp3?url=${_0xac0071}`,
+        `https://apis.davidcyriltech.my.id/download/ytmp3?url=${_0xac0071}`,
+        `https://www.dark-yasiya-api.site/download/ytmp3?url=${_0xac0071}`,
+        `https://apis.giftedtech.web.id/api/download/dlmp3?apikey=gifted&url=${_0xac0071}&apikey=gifted-md`
+      ];
+
+      // Randomly select an API from the list
       const _0x39489e = _0x5809fc.startsWith('video') 
-        ? `https://apis.davidcyriltech.my.id/download/ytmp4?url=${_0xac0071}` 
-        : `https://bandahealimaree-api-ytdl.hf.space/api/ytmp3?url=${_0xac0071}`;
+        ? _0xapiMp4[Math.floor(Math.random() * _0xapiMp4.length)]
+        : _0xapiMp3[Math.floor(Math.random() * _0xapiMp3.length)];
         
       const _0x566599 = _0x5809fc.startsWith('video') ? "video" : "audio";
       const _0x1744fd = _0x5809fc.startsWith('video') ? "video/mp4" : "audio/mpeg";
