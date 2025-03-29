a97dad1781f9af651042533b4e922110fd252963
DEVELOPER-BERA <berabruce9@gmail.com>
2025-03-29 16:07:27 +0300

    @bera

--- config.cjs
+++ config.cjs
@@ -1,3 +1,4 @@
+// config.js
 const fs = require("fs");
 require("dotenv").config();
 
@@ -13,29 +14,21 @@ const config = {
   AUTO_RECORDING: process.env.AUTO_RECORDING !== undefined ? process.env.AUTO_RECORDING === 'true' : false,
   ALWAYS_ONLINE: process.env.ALWAYS_ONLINE !== undefined ? process.env.ALWAYS_ONLINE === 'true' : false,
   AUTO_REACT: process.env.AUTO_REACT !== undefined ? process.env.AUTO_REACT === 'true' : false,
-  AUTO_BLOCK: process.env.AUTO_BLOCK !== undefined ? process.env.AUTO_BLOCK === 'true' : false,
+   /*auto block only for 212 */
+  AUTO_BLOCK: process.env.AUTO_BLOCK !== undefined ? process.env.AUTO_BLOCK === 'true' : true,
+  
+  
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "BRUCE BERA",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "254743982206",
-  GEMINI_KEY: process.env.GEMINI_KEY || "",
+  BOT_NAME: process.env.BOT_NAME || "ℂ𝕃𝕆𝕌𝔻 𝔸𝕀",
+  MENU_IMAGE: process.env.MENU_IMAGE || "https://files.catbox.moe/7l1tt5.jpg",
+  DESCRIPTION: process.env.DESCRIPTION || "© ℝ𝔼𝔾𝔸ℝ𝔻𝕊 𝔹𝔼ℝ𝔸",
+  OWNER_NAME: process.env.OWNER_NAME || "𝔹ℝ𝕌ℂ𝔼 𝔹𝔼ℝ𝔸",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "0743982206",
+  GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
-
-  // 🛡 Antileft & Antidelete Settings
-  ANTI_LEFT: process.env.ANTI_LEFT !== undefined ? process.env.ANTI_LEFT === 'true' : true, 
-  ANTI_DELETE: process.env.ANTI_DELETE !== undefined ? process.env.ANTI_DELETE === 'true' : true,
-
-  // Auto Bio with Juice WRLD Quotes
-  AUTO_BIO: process.env.AUTO_BIO !== undefined ? process.env.AUTO_BIO === 'true' : false,
-  BIO_QUOTES: [
-    "I still see your shadows in my room...",
-    "You left me falling and landing inside my grave...",
-    "I know that the truth is hard to digest...",
-    "We ain't making it past 21...",
-    "I'm a jealous boy, I really feel like John Lennon...",
-    "Took a pill in Ibiza to show Avicii I was cool..."
-  ]
 };
+
 
 module.exports = config;
